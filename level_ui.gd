74a58e8e454214331e96dec268bd2de9dab57386
Eunhyeok Kwon <eunhyeoq@gmail.com>
2025-04-19 00:40:07 +0900

    Edit layout

--- level_ui.gd
+++
@@ -1,9 +0,0 @@
-extends MarginContainer
-
-func _ready():
-	var margin_value = 15
-	add_theme_constant_override("margin_bottom", margin_value)
-	add_theme_constant_override("margin_left", margin_value)
-	#add_theme_constant_override("margin_right", margin_value)
-	add_theme_constant_override("margin_top", margin_value)
-	
